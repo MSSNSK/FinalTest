c29ad9e4af6253cf9dafb73b0d13a8f5bbc3e798
Markov Sergei <irbis_mssw@mail.ru>
2022-10-03 06:54:24 +0700

    Add Task in TaskFinalTest directoty

---
+++ TaskFinalTest/Task.cs
@@ -0,0 +1,36 @@
+﻿
+string[] GetArray(int size)
+{
+    string[] result = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i + 1} element of the array: ");
+        result[i] = Console.ReadLine();
+    }
+    return result;
+}
+
+string[] SortArray(string[] array)
+{
+    string result = String.Empty;
+    int elementLength = 3;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= elementLength)
+        {
+            if (result.Length > 0) result += ", ";
+            result += $"{array[i]}";
+        }
+    }   
+    return result.Split(", ");
+}
+
+Console.Write("Enter the number of array elements: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = GetArray(sizeArray);
+string[] newArray = SortArray(myArray);
+
+Console.WriteLine($"[{string.Join(", ", myArray)}] -> [{string.Join(", ", newArray)}]");
